a79750ed0a2280516200ca479e40da99ba646bbb
cxcristian <cristiandavid3d@gmail.com>
2026-05-20 09:56:30 -0500

    clase usuarios en el gestor, settings para los valores env.

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,1 +1,13 @@
 #inicializacion
+#Leer las variables de entorno
+from dotenv import load_dotenv as ld
+import os
+ld()
+APP_NAME= os.getenv("APP_NAME")
+APP_VERSION= os.getenv("APP_VERSION")
+ADMIN_USER= os.getenv("ADMIN_USER")
+ADMIN_PASSWORD= os.getenv("ADMIN_PASSWORD")
+
+print(f"Nombre: {APP_NAME}")
+print(f"Versión: {APP_VERSION}")
+print(f"Usuario admin: {ADMIN_USER}")
--- app/usuarios/gestor.py
+++ app/usuarios/gestor.py
@@ -1,1 +1,16 @@
-#inicializacion
+##=============Clase usuarios ============
+class DataBase:
+    def __init__(self):
+        self.usuarios= []
+    def registrar_usuario(self, usuario):
+        self.usuarios.append(usuario)
+
+    def listar_usuarios(self):
+        for usuario in self.usuarios:
+            print(usuario)
+            
+    def obtener_usuario(self, nombre):
+        for usuario in self.usuarios:
+            if usuario.nombre == nombre:
+                return usuario
+        return None
